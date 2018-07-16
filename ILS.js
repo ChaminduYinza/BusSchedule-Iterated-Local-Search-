5b702c93457b7ea02ab37f5240b07f62949b5ac2
RPA BOT02 <rpa02@nations.int>
2018-07-16 18:15:50 +0530

    rest json update

--- ILS.js
+++ ILS.js
@@ -3,71 +3,95 @@ busArray = [{
     name: "bus1",
     noOfSeats: 40,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus2",
     noOfSeats: 47,
     route: "120",
-    length: 18
+    length: 18,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus3",
     noOfSeats: 40,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus4",
     noOfSeats: 50,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus5",
     noOfSeats: 45,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus6",
     noOfSeats: 30,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus7",
     noOfSeats: 30,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus8",
     noOfSeats: 30,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus9",
     noOfSeats: 30,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 },
 {
     name: "bus10",
     noOfSeats: 30,
     route: "120",
-    length: 20
+    length: 20,
+    totalRevenue: 0,
+    numberOfTurns: 0
 }]
 
-var globalBest = 0;
-// var currentBestSolution = [];
+var globalBest = -100;
 var returnJSON = {
-    busAllocation: [],
+    allocation: {
+        busAllocation: [],
+        busIDs: []
+    },
     message: ""
 };
 
-console.log(generateSchedule(new Date("October 13, 2014 08:11:00"), new Date("October 13, 2014 09:20:00"), 20, 5, [40, 20, 40], busArray, 10));
+
+
+console.log(generateSchedule(new Date("October 13, 2014 08:11:00"), new Date("October 13, 2014 09:20:00"), 20, 5, [30, 100, 40], busArray, 10));
 
 function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPassengerCount, busArray, maxItarationCount) {
     let allSolutions = [];
@@ -82,7 +106,7 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
         return [{ message: "Passenger average array is not enough to cover an optimal output\nNo of Slots : " + noOfSlots + "\nPassenger Average Array : " + avgPassengerCount.length }];
     }
     for (let r = 0; r < maxItarationCount; r++) {
-        let previousGlobalSolution = JSON.parse(JSON.stringify(returnJSON.busAllocation));
+        let previousGlobalSolution = JSON.parse(JSON.stringify(returnJSON.allocation.busAllocation));
         let validationCount = 0;
         do {
             initialSolution = generateInitialSolution(noOfSlots, noOfBusses);
@@ -92,8 +116,9 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
                 validationCount = validationCount + 1
                 //All the possible solutions are generated and evaluvated
                 if (validationCount > allSolutions.length + 100) {
-                    if (globalBest == 0) {
-                        returnJSON.message = "There is no optimal solution available for the given inputs this is just a random solution"
+                    if (globalBest <= 0) {
+                        // Generated solution is optimal for the given output But the number of busses are more than enough
+                        returnJSON.message = "Dummy Message"
                         return returnJSON
                     } else {
                         return returnJSON
@@ -115,7 +140,7 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
 
         allSolutions.push(swapSolutionElements(initialSolution, avgPassengerCount, busArray));
 
-        if (returnJSON.busAllocation.toString() == previousGlobalSolution.toString()) {
+        if (returnJSON.allocation.busAllocation.toString() == previousGlobalSolution.toString()) {
             count++;
         } else {
             count = 0;
@@ -123,8 +148,8 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
 
         if (count > (maxItarationCount / 2)) {
 
-            if (globalBest == 0) {
-                returnJSON.message = "There is no optimal solution available for the given inputs this is just a random solution"
+            if (globalBest <= 0) {
+                returnJSON.message = "Dummy Message"
                 return returnJSON
 
 
@@ -134,8 +159,8 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
         }
     }
 
-    if (globalBest == 0) {
-        returnJSON.message = "There is no optimal solution available for the given inputs this is just a random solution"
+    if (globalBest <= 0) {
+        returnJSON.message = "Dummy Message"
         return returnJSON
 
 
@@ -143,6 +168,13 @@ function generateSchedule(startTime, endTime, fixedInterval, noOfBusses, avgPass
         return returnJSON
     }
 
+
+}
+
+function setReturnJSON(solution, passengerAverage) {
+    for (let i = 0; i < solution.length; i++) {
+
+    }
 
 }
 
@@ -208,11 +240,11 @@ function swapSolutionElements(originalSolution, passengerAverage, busArray) {
         for (let r = i; r < originalSolution.length; r++) {
             let fitnessValue = evaluvateSolution(solution, passengerAverage, busArray);
             if (globalBest == fitnessValue) {
-                returnJSON.busAllocation = JSON.parse(JSON.stringify(solution));
+                returnJSON.allocation.busAllocation = JSON.parse(JSON.stringify(solution));
             }
             if (globalBest < fitnessValue) {
                 globalBest = fitnessValue;
-                returnJSON.busAllocation = JSON.parse(JSON.stringify(solution));
+                returnJSON.allocation.busAllocation = JSON.parse(JSON.stringify(solution));
             }
             if (r != solution.length - 1) {
                 let temp = solution[i];
@@ -223,45 +255,72 @@ function swapSolutionElements(originalSolution, passengerAverage, busArray) {
         }
 
     }
-
-    return returnJSON.busAllocation;
+    return returnJSON.allocation.busAllocation;
 }
 
 
 function evaluvateSolution(solution, passengerAverage, busArray) {
     let spliceIndex = 0;
-    let spliceAmount = 0;
     let finessValue = 0;
-    // console.log(solution)
 
     for (let i = 0; i < solution.length; i++) {
         let sumOfSeats = 0;
-        let numberOfBusses = solution[i]; //3
         let spliceArray = JSON.parse(JSON.stringify(busArray));
 
         let newBusArray = spliceArray.splice(spliceIndex, solution[i])
         spliceIndex = spliceIndex + solution[i];
         newBusArray = JSON.parse(JSON.stringify(newBusArray));
-        // console.log(newBusArray[0].noOfSeats)
 
 
         for (let r = 0; r < newBusArray.length; r++) {
             sumOfSeats = sumOfSeats + newBusArray[r].noOfSeats;
         }
 
+        let slack = sumOfSeats - passengerAverage[i];
+
+        if (slack < -10) {
+            finessValue = finessValue - 2;
+        } else if (slack < 0) {
+            finessValue = finessValue - 1;
+        } else if (slack > 100) {
+            finessValue = finessValue - 50;
+        } else if (slack > 90) {
+            finessValue = finessValue - 40;
+        } else if (slack > 80) {
+            finessValue = finessValue - 30;
+        } else if (slack > 70) {
+            finessValue = finessValue - 20;
+        } else if (slack > 60) {
+            finessValue = finessValue - 10;
+        } else if (slack > 50) {
+            finessValue = finessValue - 5;
+        } else if (slack > 40) {
+            finessValue = finessValue - 4;
+        } else if (slack > 30) {
+            finessValue = finessValue - 3;
+        } else if (slack > 20) {
+            finessValue = finessValue - 1;
+        } else if (slack > 5) {
+            finessValue = finessValue + 2;
+        } else if (slack > 0) {
+            finessValue = finessValue + 1;
+        }
 
         //take the difference instead
-        if (sumOfSeats >= passengerAverage[i] + 17) {
-            finessValue = finessValue - 1
-        } else if (sumOfSeats >= passengerAverage[i] + 16) {
-            finessValue = finessValue
-        } else if (sumOfSeats >= passengerAverage[i] + 10 && sumOfSeats < passengerAverage[i] + 15) {
-            finessValue = finessValue + 2
-        } else if (sumOfSeats > passengerAverage[i]) {
-            finessValue = finessValue + 1
-        } else if (sumOfSeats < passengerAverage[i]) {
-            finessValue = finessValue - 2
-        }
+        // if (sumOfSeats >= passengerAverage[i] + 17) {
+        //     finessValue = finessValue - 1
+        // } else if (sumOfSeats >= passengerAverage[i] + 16) {
+        //     finessValue = finessValue
+        // } else if (sumOfSeats >= passengerAverage[i] + 10 && sumOfSeats < passengerAverage[i] + 15) {
+        //     finessValue = finessValue + 2
+        // } else if (sumOfSeats > passengerAverage[i]) {
+        //     finessValue = finessValue + 1
+        // } else if (sumOfSeats < passengerAverage[i]) {
+        //     finessValue = finessValue - 2
+        // }
+
+
+
         // if (sumOfSeats >= passengerAverage[i] + 50) {
         //     finessValue = finessValue - 3
         // } else if (sumOfSeats >= passengerAverage[i] + 17) {
